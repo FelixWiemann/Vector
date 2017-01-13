8cf2b9d155393ba7bfe7ed5e144775d9cf40f407
FelixWiemann <felix.wiemann@web.de>
2017-01-13 21:49:28 +0100

    extracted line, plane, point

---
+++ Vector/Line.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nepumuk
+{
+    public struct Line
+    {
+
+        #region Vars
+        private Vector zeroVector;
+        private Vector directionalVector;
+        #endregion
+
+        #region constructors
+
+        public Line(int dim)
+        {
+            zeroVector = new Vector(dim);
+            directionalVector = new Vector(dim);
+        }
+        /// <summary>
+        /// Declare a line based on a zero vector and an directional vector
+        /// </summary>
+        /// <param name="zeroVector"></param>
+        /// <param name="directionalVector"></param>
+        public Line(Vector zeroVector, Vector directionalVector)
+        {
+            this.zeroVector = zeroVector;
+            this.directionalVector = directionalVector;
+        }
+
+        public Line(Point point1, Point point2)
+        {
+            zeroVector = point1.Coordinates;
+            directionalVector = Point.GetVectorToPoint(point1, point2);
+        }
+
+        /// <summary>
+        /// Defines a line through origin
+        /// </summary>
+        /// <param name="directionalVector"></param>
+        public Line(Vector directionalVector)
+        {
+            this.zeroVector = Vector.vOrigin;
+            this.directionalVector = directionalVector;
+        }
+
+        #endregion
+
+        #region get/set
+        public Vector ZeroVector
+        {
+            set { zeroVector = value; }
+            get { return zeroVector; }
+        }
+        public Vector DirectionalVector
+        {
+            set { directionalVector = value; }
+            get { return directionalVector; }
+        }
+        #endregion
+
+        #region calc
+
+        // TODO rotate around, calc distance, schneiden sich, windschief, parallel, 
+        // schnittpunktberechnung
+
+        #endregion
+
+
+    }
+ 
+}
---
+++ Vector/Point.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nepumuk
+{
+    public struct Point
+    {
+        private Vector coordinates;
+        public Point(Vector point)
+        {
+            coordinates = point;
+        }
+        public Point(double[] coordinates)
+        {
+            this.coordinates = new Vector(coordinates);
+        }
+
+        public Vector Coordinates
+        {
+            get { return coordinates; }
+            set { coordinates = value; }
+        }
+
+        #region calc
+
+        /// <summary>
+        /// get the vector that describes the direction and length you have to go from point one to point two
+        /// </summary>
+        /// <param name="mp1">point one</param>
+        /// <param name="mp2">point two</param>
+        /// <returns></returns>
+        public static Vector GetVectorToPoint(Point mp1, Point mp2)
+        {
+            return mp2.Coordinates - mp1.Coordinates;
+        }
+
+        /// <summary>
+        /// get the vector that describes the direction and length you have to go to point two
+        /// </summary>
+        /// <param name="mp2"></param>
+        public void GetVectorToPoint(Point mp2)
+        {
+            this = new Point(GetVectorToPoint(this, mp2));
+        }
+
+        /// <summary>
+        /// Calculate the distance between points
+        /// </summary>
+        /// <param name="mp1">point one</param>
+        /// <param name="mp2">point two</param>
+        /// <returns>double distance</returns>
+        public static double CalcDistanceTo(Point mp1, Point mp2)
+        {
+            //set the distance between both points as a vector and calculate the length of the vector
+            return Vector.getLength(mp1.Coordinates - mp2.Coordinates);
+        }
+        #endregion
+    }
+
+}
